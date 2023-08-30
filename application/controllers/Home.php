aa643da72690ccdee3f0da2df774a15d96515109
ziigzagz <iffan.hym@gmail.com>
2023-08-30 18:38:20 +0700

    Initial commit

---
+++ application/controllers/Home.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Comp_part');
+        $this->load->model('User');
+        date_default_timezone_set("Asia/Bangkok");
+        // if ($this->session->userdata('flowshoping_id') == null) {
+        //     redirect('Login');
+        // }
+    }
+    public function index()
+    {
+        $this->load->view('css');
+        $this->load->view('js');
+        $this->load->view('Home');
+    }
+}
