751ee493e2d1ee3371736f4b914679909cd07ea9
ziigzagz <iffan.hym@gmail.com>
2023-09-06 15:39:53 +0700

    add home page

---
+++ application/controllers/Requester.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Requester extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		date_default_timezone_set("Asia/Bangkok");
+	}
+	public function index()
+	{
+		$this->load->view('css');
+		$this->load->view('js');
+		$this->load->view('Requester\Index.php');
+	}
+}
